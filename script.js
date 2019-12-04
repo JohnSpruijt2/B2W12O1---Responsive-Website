050dd4e71bcc4778166254128fc6b49e3b03f961
John Spruijt <99058314@mydavinci.nl>
2019-12-04 10:10:51 +0100

    added slider

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ function smallMenu() {
         document.getElementById("smallcontent").style.display = "none"
 }
 }
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  window.timer = setTimeout(showSlides, 5000); // Change image every 2 seconds
+} 
+
+// Next/previous controls
+function plusSlides(n) {
+    clearTimeout(timer);
+    showSlides(slideIndex + n);
+  }
+  function currentSlide(n) {
+    clearTimeout(timer);
+    showSlides(slideIndex = n);
+  }
